713d20fb35f02c7399758ae69e34d1d2f322eb30
mploythai <110638329+mploythai@users.noreply.github.com>
2023-06-05 20:47:38 -0700

    Fixed redirect

--- app/_components/AuthForm.tsx
+++ app/_components/AuthForm.tsx
@@ -6,13 +6,16 @@ import { ThemeSupa } from "@supabase/auth-ui-shared";
 
 export default function AuthForm() {
   const supabaseClient = createClientComponentClient();
+  const origin = process.env.NEXT_PUBLIC_VERCEL_URL
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : "http://localhost:3000";
 
   return (
     <Auth
       supabaseClient={supabaseClient}
       appearance={{ theme: ThemeSupa }}
       providers={["discord"]}
-      redirectTo={`http://localhost:3000/auth/callback`}
+      redirectTo={`${origin}/auth/callback`}
       onlyThirdPartyProviders
     />
   );
