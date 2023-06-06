840f6b54847c8396169fc8e728afdfbe9f44c30e
mploythai <110638329+mploythai@users.noreply.github.com>
2023-06-06 00:54:09 -0700

    Realized I'm not using vercel links anymore lol

--- app/_components/AuthForm.tsx
+++ app/_components/AuthForm.tsx
@@ -9,13 +9,9 @@ import { useEffect } from "react";
 export default function AuthForm() {
   const supabase = createClientComponentClient();
   const router = useRouter();
-  const liveUrl = `https://${
-    process.env.NEXT_PUBLIC_DOMAIN_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL
-  }`;
-  const origin =
-    process.env.NEXT_PUBLIC_DOMAIN_URL || process.env.NEXT_PUBLIC_VERCEL_URL
-      ? liveUrl
-      : "http://localhost:3000";
+  const origin = process.env.NEXT_PUBLIC_DOMAIN_URL
+    ? `https://${process.env.NEXT_PUBLIC_DOMAIN_URL}`
+    : "http://localhost:3000";
 
   useEffect(() => {
     supabase.auth.onAuthStateChange((event, _) => {
