ef26116d489e1774bec99e4cdd1f80d0e663ee0e
mploythai <110638329+mploythai@users.noreply.github.com>
2023-06-06 00:23:11 -0700

    Jerri will be saved for another day.

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ import AuthForm from "./_components/AuthForm";
 
 export default function Home() {
   return (
-    <section className="mx-auto flex w-full max-w-screen-sm flex-1 flex-col justify-center gap-4">
+    <section className="mx-auto flex w-full max-w-screen-sm flex-1 flex-col justify-center gap-4 p-4">
       <hgroup className="flex flex-col gap-2 text-center">
-        <h1 className="text-4xl font-semibold">Welcome to Jerri.</h1>
-        <p>A better braindumping experience.</p>
+        <h1 className="text-4xl font-semibold">Mike's Braindumper</h1>
+        <p>An extremely minimal braindumping experience.</p>
       </hgroup>
 
       <AuthForm />
