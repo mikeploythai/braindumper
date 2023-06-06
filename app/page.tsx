0d99e3bb14419f5e5f203fff46c1f058bf9f20cf
mploythai <110638329+mploythai@users.noreply.github.com>
2023-06-06 00:25:12 -0700

    Stupid linting cringe + removed jerri refs

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <section className="mx-auto flex w-full max-w-screen-sm flex-1 flex-col justify-center gap-4 p-4">
       <hgroup className="flex flex-col gap-2 text-center">
-        <h1 className="text-4xl font-semibold">Mike's Braindumper</h1>
+        <h1 className="text-4xl font-semibold">Mike&apos;s Braindumper</h1>
         <p>An extremely minimal braindumping experience.</p>
       </hgroup>
 
